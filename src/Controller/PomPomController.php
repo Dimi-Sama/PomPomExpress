86e555f14fa8296027b4f5ee9f7cc7747d564497
TrueDiamant <TrueDiamant777@gmail.com>
2023-12-11 15:22:38 +0400

    dada a changer route personnage et vider index

--- src/Controller/PomPomController.php
+++ src/Controller/PomPomController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\AttributRepository;
 use App\Repository\PersonnageRepository;
+use App\Repository\VoieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class PomPomController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(PersonnageRepository $personnageRepository): Response
+    public function index(): Response
+    {
+        return $this->render('pom_pom/index.html.twig', [
+            'controller_name' => 'PomPomController',
+        ]);
+    }
+
+    #[Route('/personnage', name: 'public_personnage')]
+    public function perso(PersonnageRepository $personnageRepository, AttributRepository $attributRepository, VoieRepository $voieRepository): Response
     {
         return $this->render('pom_pom/complete.html.twig', [
             'controller_name' => 'PomPomController',
-            'personnages' => $personnageRepository->findAll()
+            'personnages' => $personnageRepository->findAll(),
+            'attributs' => $attributRepository->findAll(),
+            'voies' => $voieRepository->findAll(),
         ]);
     }
 }
